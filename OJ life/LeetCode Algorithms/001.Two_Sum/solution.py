fd7ab45eed0063bfd96651b48e44d7847346d56e
kdwycz <kdwycz@gmail.com>
2015-08-24 00:46:01 +0800

    Add: LeetCode

---
+++ OJ life/LeetCode Algorithms/001.Two_Sum/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        l = len(nums)
+        snums = set(nums)
+        dnums = nums[::-1]
+        for k,v in enumerate(nums):
+            if target-v in snums:
+                k2 = l - dnums.index(target-v) - 1
+                if k != k2:
+                    return (min(k+1,k2+1),max(k+1,k2+1))
+
+
+a = [3,2,4]
+b = 6
+s = Solution()
+print(Solution.twoSum(a, b))
